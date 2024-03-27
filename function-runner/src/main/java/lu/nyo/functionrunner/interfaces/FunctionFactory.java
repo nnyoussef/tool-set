a53c021f07adfb6f143ebb17ab63f0625c97f66f
nyoussef <nyoussef>
2024-03-27 23:28:32 +0100

    Improving Functionalities

--- function-runner/src/main/java/lu/nyo/functionrunner/interfaces/FunctionFactory.java
+++ function-runner/src/main/java/lu/nyo/functionrunner/interfaces/FunctionFactory.java
@@ -4,16 +4,16 @@ import static java.lang.String.format;
 
 public abstract class FunctionFactory<TOKEN> {
 
-    protected abstract <T extends ExecutionUnit> T resolve(TOKEN tClass);
+    protected abstract <T extends ExecutionUnit> T resolve(TOKEN token);
 
-    public final <T extends ExecutionUnit> T get(TOKEN tClass) throws ClassNotFoundException {
+    public final <T extends ExecutionUnit> T get(TOKEN token) throws ClassNotFoundException {
         try {
-            T t = resolve(tClass);
+            T t = resolve(token);
             if (t == null)
                 throw new ClassNotFoundException("Class couldn't be resolved");
             return t;
         } catch (Exception e) {
-            throw new ClassNotFoundException(format("%s Cannot resolve %s", this.getClass().getCanonicalName(), tClass));
+            throw new ClassNotFoundException(format("%s Cannot resolve %s", this.getClass().getCanonicalName(), token));
         }
     }
 
